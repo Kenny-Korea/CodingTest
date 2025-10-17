83058edeadfdc087f5f733e500f35388a6f5a47b
kenny <kmkim4238@gmail.com>
2025-10-17 09:47:49 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+export default function App() {
+  return (
+    <div className="h-screen w-screen">
+      <div className="">hello world</div>
+    </div>
+  );
+}
